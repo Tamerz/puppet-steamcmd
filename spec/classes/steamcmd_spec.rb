c3346647cafc1f8cc193f9307b7d4b1609eb50a2
Tames McTigue <tames.mctigue@northwestern.edu>
2019-01-21 12:20:07 +0300

    Replaced tabs with 2 spaces

--- spec/classes/steamcmd_spec.rb
+++ spec/classes/steamcmd_spec.rb
@@ -1,55 +1,55 @@
 require 'spec_helper'
 
 describe 'steamcmd' do
-	test_on = {
-		:facterversion  => '2.4',
-		:hardwaremodels => ['x86_64', 'i386']
-	}
+  test_on = {
+    :facterversion  => '2.4',
+    :hardwaremodels => ['x86_64', 'i386']
+  }
 
   on_supported_os(test_on).each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
-			
-			case os_facts[:kernel]
-			when 'windows'
-				it { should contain_file('c:/steamcmd').with({
-					'ensure' => 'directory'})
-				}
+      
+      case os_facts[:kernel]
+      when 'windows'
+        it { should contain_file('c:/steamcmd').with({
+          'ensure' => 'directory'})
+        }
 
-				it { should contain_archive('installer').with({
-					'ensure'       => 'present',
-					'path'         => 'c:/temp/steamcmd.zip',
-					'extract'      => true,
-					'extract_path' => 'c:/steamcmd',
-					'source'       => 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip',
-					'creates'      => 'c:/steamcmd/steamcmd.exe',
-					'cleanup'      => true})
-				}
+        it { should contain_archive('installer').with({
+          'ensure'       => 'present',
+          'path'         => 'c:/temp/steamcmd.zip',
+          'extract'      => true,
+          'extract_path' => 'c:/steamcmd',
+          'source'       => 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip',
+          'creates'      => 'c:/steamcmd/steamcmd.exe',
+          'cleanup'      => true})
+        }
 
-			when 'Linux'
-				it { should contain_file('/opt/steamcmd').with({
-					'ensure' => 'directory'})
-				}
+      when 'Linux'
+        it { should contain_file('/opt/steamcmd').with({
+          'ensure' => 'directory'})
+        }
 
-				it { should contain_user('steamcmd') }
+        it { should contain_user('steamcmd') }
 
-				case os_facts[:os]['family']
-				when 'RedHat'
-					if os_facts[:architecture] == 'x86_64'
-						it { should contain_package('glibc.i686') }
-						it { should contain_package('libstdc++.i686') }
-					elsif os_facts[:architecture] == 'i386'
-						it { should contain_package('glibc') }
-						it { should contain_package('libstdc++') }
-					end
-				when 'Debian'
-					if os_facts[:architecture] == 'x86_64'
-						it { should contain_package('lib32gcc1') }
-					end
-				end
-			end
+        case os_facts[:os]['family']
+        when 'RedHat'
+          if os_facts[:architecture] == 'x86_64'
+            it { should contain_package('glibc.i686') }
+            it { should contain_package('libstdc++.i686') }
+          elsif os_facts[:architecture] == 'i386'
+            it { should contain_package('glibc') }
+            it { should contain_package('libstdc++') }
+          end
+        when 'Debian'
+          if os_facts[:architecture] == 'x86_64'
+            it { should contain_package('lib32gcc1') }
+          end
+        end
+      end
 
     end
   end
